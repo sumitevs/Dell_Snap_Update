764ade82bb77a5003649787c1eba6eea9a78305b
Sumit Saseendran <Sumit_Saseendran@Dell.com>
2020-03-29 20:18:24 +0530

    Add project files.

---
+++ DellSnapUpdate/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DellSnapUpdate
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            string appName = Windows.ApplicationModel.Package.Current.DisplayName;
+            AppTitle.Text = appName;
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                //ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                switch (item.Tag.ToString())
+                {
+                    case "Home":
+                        //ContentFrame.Navigate(typeof(SettingsPage));
+                        break;
+                    case "About":
+                        NavView.Header = "About Snap Update...";
+                        //ContentFrame.Navigate(typeof(SettingsPage));
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
