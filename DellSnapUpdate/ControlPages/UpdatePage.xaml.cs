ecadec16c3bb68d5bc594a248c345ead9b13b1ac
Debopriya Bhattacharjee <Debopriya_Bhattachar@Dell.com>
2020-04-11 15:45:54 +0530

    Update Page with Update ViewModel and runFullTrust capability

--- DellSnapUpdate/ControlPages/UpdatePage.xaml.cs
+++ DellSnapUpdate/ControlPages/UpdatePage.xaml.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using DellSnapUpdate.DataProvider;
+using DellSnapUpdate.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +30,60 @@ namespace DellSnapUpdate.ControlPages
     /// </summary>
     public sealed partial class UpdatePage : Page
     {
+        public UpdateViewModel ViewModel { get; set; }
+
+        private static CoreDispatcher _coreDispatcher;
+
+        public delegate void ListenerEventHandler(IStorageQueryResultBase sender, object args);
+
         public UpdatePage()
         {
+
+            ViewModel = new UpdateViewModel();
             this.InitializeComponent();
+            this.Loaded += UpdatePage_Loaded;
+            _coreDispatcher = Window.Current.Dispatcher;
+        }
+
+
+        private async void UpdatePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var listenerEventHandler = new ListenerEventHandler(resultSet_ContentsChanged);
+            await ViewModel.ConfigureFolderListener(listenerEventHandler);
+
+        }
+
+        private async void ButtonScan_Click(object sender, RoutedEventArgs e)
+        {
+            //Launching exe to generate IC Report
+            await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync("ICGroup");
+
+            ViewModel.IsLoading = true;
+            loadingText.Visibility = Visibility.Visible;
+            scanStack.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var updateInfos = driverList.ViewModel.UpdateInfos;
+            var checkedUpdate = updateInfos.Where(dr => dr.IsChecked == true).ToList();
+        }
+
+        //Event Handler for the folder listener
+        private async void resultSet_ContentsChanged(IStorageQueryResultBase sender, object args)
+        {
+            await driverList.ViewModel.LoadAsync();
+
+            //CoreDispatcher for running in UI thread
+            await _coreDispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                ViewModel.IsLoading = false;
+
+                driverList.Visibility = Visibility.Visible;
+                updateStack.Visibility = Visibility.Visible;
+                loadingText.Visibility = Visibility.Collapsed;
+            });
+
         }
     }
 }
---
+++ DellSnapUpdate/ViewModel/UpdateViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DellSnapUpdate.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Search;
+using static DellSnapUpdate.ControlPages.UpdatePage;
+
+namespace DellSnapUpdate.ViewModel
+{
+    public class UpdateViewModel : Observable
+    {
+        private bool isLoading;
+
+        public bool IsLoading
+        {
+            get => isLoading;
+            set
+            {
+                isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task ConfigureFolderListener(ListenerEventHandler listenerEventHandler)
+        {
+            //Add capability "broadFileSystemAccess" in Package.appxmanifest  
+            //In Settings -> Privacy -> File System grant access to this App
+            var storageFolder = await StorageFolder.GetFolderFromPathAsync(@"C:\Users\Debopriya_Bhattachar\Desktop\ic\IC");
+            var supportedExtension = new List<string> { ".xml" };
+            QueryOptions option = new QueryOptions(CommonFileQuery.DefaultQuery, supportedExtension);
+            option.FolderDepth = FolderDepth.Shallow;
+            option.IndexerOption = IndexerOption.UseIndexerWhenAvailable;
+            StorageFileQueryResult resultSet = storageFolder.CreateFileQueryWithOptions(option);
+            await resultSet.GetFilesAsync(0, 1);
+            resultSet.ContentsChanged += new TypedEventHandler<IStorageQueryResultBase, object>(listenerEventHandler);
+        }
+    }
+}
