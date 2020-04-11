233511b83ca4f4946378bd493f0cb475d076a1ea
Debopriya Bhattacharjee <Debopriya_Bhattachar@Dell.com>
2020-04-11 12:50:53 +0530

    Add DriverListViewModel

---
+++ DellSnapUpdate/ViewModel/DriverListViewModel.cs
@@ -0,0 +1,47 @@
+﻿using DellSnapUpdate.DataProvider;
+using DellSnapUpdate.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace DellSnapUpdate.ViewModel
+{
+    public class DriverListViewModel
+    {
+        private IUpdateInfoProvider _updateInfoProvider;
+        public ObservableCollection<UpdateInfo> UpdateInfos { get; }
+
+        private static CoreDispatcher _coreDispatcher;
+
+        public DriverListViewModel(IUpdateInfoProvider updateDataProvider)
+        {
+            _updateInfoProvider = updateDataProvider;
+            UpdateInfos = new ObservableCollection<UpdateInfo>();
+            _coreDispatcher = Window.Current.Dispatcher;
+        }
+
+        public async Task LoadAsync()
+        {
+
+            var updates = await Task.Run(() => new UpdateInfoProvider().GetSwbUpdatesAsync());
+
+            var isUIThread = _coreDispatcher.HasThreadAccess;
+
+            await _coreDispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                UpdateInfos.Clear();
+                foreach (var update in updates)
+                {
+                    UpdateInfos.Add(update);
+                }
+
+            });
+
+        }
+    }
+}
